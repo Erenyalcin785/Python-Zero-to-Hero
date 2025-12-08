0d89f6c872a11c7fa998d6ea40d67463b7ae6bfa
Erenyalcin785 <ernylcn0101@gmail.com>
2025-12-08 21:50:17 +0300

    Update 22_Mors Alfabesi.py

--- Python Zero to Hero/22_Mors Alfabesi.py
+++ Python Zero to Hero/22_Mors Alfabesi.py
@@ -32,13 +32,3 @@ for i in range(0,len(Cümle)):
     mors_indeks = Cümle[i]
     sonuc = MorsKod.get(mors_indeks)
     print(Cümle[i],chr(26),sonuc)
-
-
-# cumle=input("Mors koduna çevireceğiniz cümleyi giriniz: ").upper()
-# mors_cumle=""
-# for i in range(0,len(cumle)):
-#     cumle_indeks=cumle[i]
-#     mors_kodu=MorsKod.get(cumle_indeks)
-#     print(cumle[i], chr(26),mors_kodu)
-#     mors_cumle+=mors_kodu + " "
-# print("mors cümlesi: ", mors_cumle)
